6f47f24f35a15f2e640c98082cb4bbb86c138f94
Elkhan <74042126+ubbeg2000@users.noreply.github.com>
2023-03-29 01:01:58 +0700

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 
 struct song
 {
-    //Variabel boleh ditambahkan, namun tidak boleh dikurangi
     char penyanyi[25];
     char judul[50];
 };
@@ -30,38 +29,158 @@ typedef struct stack_playlist
 
 } stack;
 
-//Boleh menambahkan fungsi untuk mengolah stack
 
-//Menampilkan Playlist serta lagu yang diputar berdasarkan playIndex
-void showPlaylist(stack *playlist, int playIndex)
+int isEmptyPlaylist(stack *playlist)
 {
-    //Implementasi Fungsi
-    printf("Playlist kosong! Tambahkan lagu!\n");
-
-    //printf("-> %s oleh %s",//lagu, penyanyi
-    //printf("   %s oleh %s",//lagu, penyanyi
-
+    return (playlist->top == NULL ? 1 : 0);
 }
 
+void push(stack *playlist, struct song _songData)
+{
+    node *newNode;
+    newNode = malloc(sizeof(struct node_stack));
+    newNode->songData = _songData;
+    newNode->nextNode = playlist->top;
+    playlist->top = newNode;
+}
+
+void pop(stack *playlist)
+{
+    node *tmp;
+    tmp = playlist->top;
+    playlist->top = tmp->nextNode;
+
+    free(tmp);
+}
+
+void showPlaylist(stack *playlist, int playIndex)
+{
+
+    node *tmp;
+    int i = 0;
+    tmp = playlist->top;
+
+    printf("\nNow Playing:\n");
+    if (isEmptyPlaylist(playlist))
+    {
+        printf("Playlist kosong! Tambahkan lagu!\n");
+        return;
+    }
+
+    // Reverse Stack untuk menampilkan secara FIFO
+    stack *reversed_display;
+    reversed_display = malloc(sizeof(stack *));
+    reversed_display->top = NULL;
+    while (tmp != NULL)
+    {
+        push(reversed_display, tmp->songData);
+        tmp = tmp->nextNode;
+    }
+
+    // Tampilkan playlist dari hasil reverse stack
+    tmp = reversed_display->top;
+    while (tmp != NULL)
+    {
+        if (i == playIndex)
+        {
+            printf("-> %s oleh %s",
+                   tmp->songData.penyanyi,
+                   tmp->songData.judul);
+        }
+        else
+        {
+            printf("   %s oleh %s",
+                   tmp->songData.penyanyi,
+                   tmp->songData.judul);
+        }
+        tmp = tmp->nextNode;
+        i++;
+    }
+}
+
+
 int main()
-{   
-    //Beberapa variabel yang mungkin diperlukan
+{
     stack *currPlaylist;
     char *token;
-    struct song songBuf; //Struct buffer
-    char str[75]; //Variabel Penampung Masukan Lagu dan Penyanyi
+    struct song songBuf;
+    char str[75];
+    currPlaylist = (stack *)malloc(sizeof(stack));
+    //Inisialisasi Stack
+    currPlaylist->top = NULL;
     char temp;
-    char cmd = 'X'; //Variabel penyimpan perintah
-    int numberofsong = 0; //Jumlah lagu yang tersimpan dalam playlist
-    int nowIndex = -1; //Indeks lagu yang sedang diputar
+
+    char cmd = 'X';
+    int numberofsong = 0;
+    int nowIndex = -1;
     printf("-*-_-*-_-*-_-*-Jooxtify-*-_-*-_-*-_-*-");
 
-    showPlaylist(currPlaylist, nowIndex);//Contoh penggunaan fungsi showPlaylist
+    while (cmd != 'E')
+    {
+        showPlaylist(currPlaylist, nowIndex);
 
-    printf("\nMasukkan Perintah: ");
-    printf("Masukkan Nama Lagu dan Penyanyi: ");
-    // printf("Masukan Perintah Tidak Valid!"); 
-    getchar(); //Perintah mengosongkan buffer bila diperlukan
+        printf("\nMasukkan Perintah: ");
+        scanf("%c", &cmd);
+        
+        switch(cmd){
+            case '+':
+                printf("Masukkan Nama Lagu dan Penyanyi: ");
+                scanf("%c",&temp);
+                scanf("%[^\n]", str);
+                char ch = '\n';
+                strncat(str, &ch, 1);        
+            
+                token = strtok(str, ",");
+                strcpy(songBuf.penyanyi, token);
+                token = strtok(NULL, ",");
+                strcpy(songBuf.judul, token);
 
+                //Masukkan ke dalam Stack
+                push(currPlaylist, songBuf);
+                //Update jumlah lagu
+                numberofsong++;
+                break;
+            
+            case '>':
+                if(nowIndex >= numberofsong-1){
+                    nowIndex = 0;
+                    }
+                else{
+                    nowIndex++;
+                    }
+                break;
+            case '<':
+                if(nowIndex <= 0){
+                    nowIndex = numberofsong-1;
+                }
+                else if(nowIndex >= numberofsong-1){
+                    nowIndex = numberofsong-1;
+                }
+                else
+                {
+                    nowIndex--;
+                }
+                break;
+            case 'R':
+                nowIndex = 0;
+                break;
+            case '-':
+                if(numberofsong == 0){
+                }
+                else{
+                pop(currPlaylist);
+                numberofsong--;
+                }
+
+                break;
+
+             case 'E':
+                break;
+            default:
+                printf("Masukan Perintah Tidak Valid!");
+                break;
+        }
+        getchar();
+    }
     return 0;
 }
